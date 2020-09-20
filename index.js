5fc9ccd2b802c791f27744c39c6bc258c8e83ef6
ikmokhirio <ikmokhirio@mail.ru>
2020-09-20 16:01:55 +0300

    Task №6 : Execute passed function

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function getRandomColorHexCode() { // Get RGBA hex color
 
     return rgbaColor;
 }
+
+function executeFunction(functionToExecute) { // Execute passed function
+
+    if(typeof(functionToExecute) != "function") {
+        let error = new Error("Invalid parameter passed");
+        throw(error);
+    }
+
+    functionToExecute();
+}
