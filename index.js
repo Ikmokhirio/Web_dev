7d2f17eec31289e1140def4300e6a2b090e0ea53
ikmokhirio <ikmokhirio@mail.ru>
2020-09-20 16:37:25 +0300

    Task №16 : Check if domain is correct

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ function executeFunction(functionToExecute) { // Execute passed function
 
     functionToExecute();
 }
+
+function isDomainCorrect(url) {
+    if(typeof(url) != "string") {
+        let error = new Error("Invalid parameter passed");
+        throw(error);
+    }
+
+    if(url.length < 1 && url.length > 253) return false;
+
+    let parts = url.split(".");
+
+    for(let i = 0; i < parts.length; i++) {
+
+        let part = parts[i].toLowerCase();
+
+        if (part.length > 63) return false;
+
+        if (!part.match(/^[a-z\d](-*[a-z\d])*$/)) {
+            return false;
+        }
+
+    }
+
+    return true;
+}
