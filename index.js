93af868b7918b131c2330c174fc597c50d620857
ikmokhirio <ikmokhirio@mail.ru>
2020-09-22 14:56:07 +0300

    Error codes were replaced with constants

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const functions = require("./lab1Functions");
 const mainUrl = '/api/DryavichevIvan/lab1/';
 const port = 3000;
 
+const BAD_REQUEST = 400;
+
 const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send("<h1>" +mainUrl + "functionName?key=value</h1><br>" +
+    res.send("<h1>" + mainUrl + "functionName?key=value</h1><br>" +
         "<h4>№3 " + mainUrl + "color?type=rgba</h4>" +
         "<p>Get random color in hex. Type can be RGBA or RGB</p><br><br>" +
         "<h4>№6 " + mainUrl + "execute?function=f</h4>" +
@@ -19,7 +21,7 @@ app.get(mainUrl + "execute", function (req, res) {
     let functionString = req.query.function;
 
     if (functionString === undefined) {
-        res.status(400).send("Incorrect data was passed");
+        res.status(BAD_REQUEST).send("Incorrect data was passed");
         return;
     }
 
@@ -29,14 +31,13 @@ app.get(mainUrl + "execute", function (req, res) {
     } catch (e) {
         res.send("An error occurred\n" + e.message);
     }
-
 });
 
 app.get(mainUrl + "color", function (req, res) {
     let colorType = req.query.type;
 
     if (colorType === undefined || (colorType.toLowerCase() !== 'rgba' && colorType.toLowerCase() !== 'rgb')) {
-        res.status(400).send("Incorrect data was passed");
+        res.status(BAD_REQUEST).send("Incorrect data was passed");
         return;
     }
 
@@ -44,11 +45,11 @@ app.get(mainUrl + "color", function (req, res) {
 
 });
 
-app.get(mainUrl + "domain", function (req,res) {
+app.get(mainUrl + "domain", function (req, res) {
     let domainName = req.query.address;
 
     if (domainName === undefined) {
-        res.status(400).send("Incorrect data was passed");
+        res.status(BAD_REQUEST).send("Incorrect data was passed");
         return;
     }
 
