13afec8ede2f56a8b6af7a6ae56cf99842dd44fd
ikmokhirio <ikmokhirio@mail.ru>
2020-10-03 23:24:52 +0300

    Reworked routes.js in order to make import easier

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port = 3000;
 const express = require("express");
 const hbs = require("hbs")
 const app = express();
+const routes = require('./routes');
 
 app.set('view engine', 'hbs');
 
@@ -10,7 +11,7 @@ hbs.registerPartials("./views/partials/")
 
 app.use(express.static("public"));
 
-const routes = require('./routes')(app);
+app.use(routes);
 
 app.listen(port, () => {
     console.log("SERVER STARTED AT %d PORT", port);
--- routes.js
+++ routes.js
@@ -15,125 +15,124 @@ const {
     UNAUTHORIZED
 } = require("./httpError");
 
-module.exports = function (app) {
+router = require("express").Router()
 
-    app.use(logRequestToConsole);
+router.use(logRequestToConsole);
 
-    app.get('/', function (req, res) {
-        res.render('index.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage
-        });
+router.get('/', function (req, res) {
+    res.render('index.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage
     });
+});
 
-    app.get(mainUrl + functionInputPage, function (req, res) {
-        res.render('functionInput.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            executePath: mainUrl + 'execute'
-        });
+router.get(mainUrl + functionInputPage, function (req, res) {
+    res.render('functionInput.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        executePath: mainUrl + 'execute'
     });
+});
 
-    app.get(mainUrl + "execute", authenticationCheck, function (req, res) {
-        let functionString = req.query.function;
+router.get(mainUrl + "execute", authenticationCheck, function (req, res) {
+    let functionString = req.query.function;
 
-        if (functionString === undefined) {
-            throw new HttpError(BAD_REQUEST, "Incorrect data was passed");
-        }
+    if (functionString === undefined) {
+        throw new HttpError(BAD_REQUEST, "Incorrect data was passed");
+    }
 
-        let answer = "Function was called successfully";
+    let answer = "Function was called successfully";
 
-        try {
-            functions.executeFunction(new Function(functionString));
-        } catch (e) {
-            answer = "An error occurred: " + e.message;
-        }
+    try {
+        functions.executeFunction(new Function(functionString));
+    } catch (e) {
+        answer = "An error occurred: " + e.message;
+    }
 
-        res.render('result.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            answer: answer
-        });
+    res.render('result.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        answer: answer
     });
+});
 
-    app.get(mainUrl + colorInputPage, function (req, res) {
-        res.render('colorInput.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            executePath: mainUrl + 'color'
-        });
+router.get(mainUrl + colorInputPage, function (req, res) {
+    res.render('colorInput.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        executePath: mainUrl + 'color'
     });
+});
 
-    app.get(mainUrl + "color", function (req, res) {
-        let colorType = req.query.type;
+router.get(mainUrl + "color", function (req, res) {
+    let colorType = req.query.type;
 
-        if (colorType === undefined || (colorType.toLowerCase() !== 'rgba' && colorType.toLowerCase() !== 'rgb')) {
-            throw new HttpError(BAD_REQUEST, "Incorrect data was passed");
-        }
+    if (colorType === undefined || (colorType.toLowerCase() !== 'rgba' && colorType.toLowerCase() !== 'rgb')) {
+        throw new HttpError(BAD_REQUEST, "Incorrect data was passed");
+    }
 
-        let color = functions.getRandomColorHexCode(colorType.toLowerCase() === 'rgba');
-        res.render('result.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            answer: "Your color is : " + color
-        });
+    let color = functions.getRandomColorHexCode(colorType.toLowerCase() === 'rgba');
+    res.render('result.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        answer: "Your color is : " + color
     });
+});
 
-    app.get(mainUrl + domainInputPage, function (req, res) {
-        res.render('domainInput.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            executePath: mainUrl + 'domain'
-        });
+router.get(mainUrl + domainInputPage, function (req, res) {
+    res.render('domainInput.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        executePath: mainUrl + 'domain'
     });
+});
 
-    app.get(mainUrl + "domain", authenticationCheck, function (req, res) {
-        let domainName = req.query.address;
+router.get(mainUrl + "domain", authenticationCheck, function (req, res) {
+    let domainName = req.query.address;
 
-        if (domainName === undefined) {
-            res.status(BAD_REQUEST).send("Incorrect data was passed");
-            return;
-        }
+    if (domainName === undefined) {
+        res.status(BAD_REQUEST).send("Incorrect data was passed");
+        return;
+    }
 
-        let answer = functions.isDomainCorrect(domainName) ? "Domain is correct" : "Domain in incorrect";
+    let answer = functions.isDomainCorrect(domainName) ? "Domain is correct" : "Domain in incorrect";
 
-        res.render('result.hbs', {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            answer: answer
-        });
+    res.render('result.hbs', {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        answer: answer
     });
+});
 
-    app.use(function (req, res, next) {
-        throw new HttpError(NOT_FOUND, 'Not Found');
+router.use(function (req, res, next) {
+    throw new HttpError(NOT_FOUND, 'Not Found');
+});
+
+router.use(function (err, req, res, next) {
+    res.status(err.statusCode).render("error.hbs", {
+        title: title,
+        task1: mainUrl + colorInputPage,
+        task2: mainUrl + functionInputPage,
+        task3: mainUrl + domainInputPage,
+        message: err.name + " " + err.message
     });
+    next(err);
+});
 
-    app.use(function (err, req, res, next) {
-        res.status(err.statusCode).render("error.hbs", {
-            title: title,
-            task1: mainUrl + colorInputPage,
-            task2: mainUrl + functionInputPage,
-            task3: mainUrl + domainInputPage,
-            message: err.name + " " + err.message
-        });
-        next(err);
-    });
+router.use(logErrorsToFile);
 
-    app.use(logErrorsToFile);
-
-
-}
+module.exports = router;
