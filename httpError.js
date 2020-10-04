7cfeb2c2486e9961a609cd00d5ca61077078671b
ikmokhirio <ikmokhirio@mail.ru>
2020-10-04 21:04:35 +0300

    Added internal server error code

--- httpError.js
+++ httpError.js
@@ -11,11 +11,13 @@ const FORBIDDEN = 403;
 const NOT_FOUND = 404;
 const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
+const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
     HttpError,
     FORBIDDEN,
     NOT_FOUND,
     BAD_REQUEST,
-    UNAUTHORIZED
+    UNAUTHORIZED,
+    INTERNAL_SERVER_ERROR
 };
