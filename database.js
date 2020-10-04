b9e326322a2c08f66c17fc9c152e168b8f6e79f4
ikmokhirio <ikmokhirio@mail.ru>
2020-10-04 21:05:14 +0300

    Created function for working with database via mongoose

---
+++ database.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const uri = require('./config/keys').mongodb.uri
+const User = require("./user").User
+const argon = require("argon2")
+
+
+async function addUserToDatabase(username, password) {
+
+    const hashedPassword = await argon.hash(password);
+    const userUpload = new User({
+        username: username,
+        password: hashedPassword
+    });
+
+    console.log(hashedPassword);
+
+    argon.verify(hashedPassword, password).then(() => {
+        console.log('Successful password supplied!');
+
+        userUpload.save().then(() => {
+            console.log("SUCCESSFUL UPLOAD");
+        });
+
+        return userUpload
+
+    }).catch((err) => {
+        console.err(err + ' Invalid password supplied!');
+        return userUpload;
+    });
+}
+
+
+function initDatabase() {
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+        console.log("SUCCESSFUL CONNECT TO DATABASE");
+    });
+}
+
+exports.initDatabase = initDatabase;
+exports.uploadUserToDatabase = addUserToDatabase;
+
